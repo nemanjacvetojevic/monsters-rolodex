4b686a1023a1d24b13d1bb63ae353b42d829b19b
Nemanja Cvetojevic <nemanjacvetojevicdec@gmail.com>
2019-12-16 16:44:54 +0100

    Updates

---
+++ precache-manifest.936d7df65d678c2eea223a71fb92a451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29390e6c26e8622578d499a9c166734",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2520a7d95c1c4d96606e",
+    "url": "/monsters-rolodex/static/css/main.21352796.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2520a7d95c1c4d96606e",
+    "url": "/monsters-rolodex/static/js/main.95ce1b81.chunk.js"
+  },
+  {
+    "revision": "8f564ee1c346f305c76d",
+    "url": "/monsters-rolodex/static/js/runtime-main.17d07582.js"
+  }
+]);
